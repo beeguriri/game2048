5e7940db7754492f5341d6faf916ceb3212bee28
wendy <wendy@gridwiz.com>
2023-12-08 14:22:12 +0900

    기본설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,49 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'import', '@typescript-eslint', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+      },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+    "linebreak-style": 0,
+    "react/react-in-jsx-scope": 0,
+    "comma-dangle": 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/no-unresolved": 0,
+    "no-unused-expressions": ["error", { allowShortCircuit: true }],
+    "react/jsx-props-no-spreading": 0,
+    "consistent-return": 2,
+    "no-shadow": 0,
+    "no-underscore-dangle": 0,
+    "no-console": 2,
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@layout': path.resolve(__dirname, 'src/layout'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@style': path.resolve(__dirname, 'src/style'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
+  },
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+});
