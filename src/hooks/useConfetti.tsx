fcb7bd36b7201e4584a1369817ab7d3b47a66d7e
wendy <wendy@gridwiz.com>
2023-12-15 13:38:30 +0900

    timeout 이상해

--- src/hooks/useConfetti.tsx
+++ src/hooks/useConfetti.tsx
@@ -10,25 +10,33 @@ export default function useConfetti({ board }: { board: number[][] }) {
   const id = 'confetti';
 
   // react-spring을 사용하여 confetti 애니메이션 효과 적용
+  const confettiStyle = {
+    left: window.innerWidth / 2,
+    top: window.innerHeight / 2,
+    transform: 'translate(-50%, -50%)', // 중앙 정렬을 위해 필요한 스타일
+    zIndex: 999, // 다른 요소 위에 표시
+  };
+
   const confettiProps = useSpring({
-    opacity: showConfetti ? 1 : 0,
+    opacity: showConfetti ? 0.5 : 0,
     display: showConfetti ? 'block' : 'none',
   });
 
   // board에 2048이 생기면 confetti 효과 활성화
-  useEffect(() => {
-    if (board.flat().includes(2048)) setShowConfetti(true);
-
-    //3초 후 비활성화
-    const timeout = setTimeout(() => {
-      setShowConfetti(false);
-    }, 3000);
-
-    return () => clearTimeout(timeout);
-  }, [board]);
+  // useEffect(() => {
+    
+  // }, [board]);
 
   // confetti 효과 끝나면 토스트 메시지 표시
   useEffect(() => {
+    if (board.flat().includes(2048)) setShowConfetti(true);
+
+    console.log('showConfetti: ', showConfetti)
+    //3초 후 비활성화
+    const timeout = setTimeout(() => {
+      setShowConfetti(false);
+    }, 5000);
+
     if (!showConfetti && board.flat().includes(2048)) {
       if (!toast.isActive(id)) {
         toast({
@@ -43,12 +51,18 @@ export default function useConfetti({ board }: { board: number[][] }) {
         });
       }
     }
+
+    return () => clearTimeout(timeout);
   }, [showConfetti, board, toast]);
 
   return (
     <>
       <animated.div style={confettiProps}>
-        <Confetti />
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={confettiStyle}
+        />
       </animated.div>
     </>
   );
